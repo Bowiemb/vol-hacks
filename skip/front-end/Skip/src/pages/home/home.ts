59bb4775d42d712f879d5d7733fb425ef5e1ae18
Jellylemon <haku4495@gmail.com>
2018-09-30 06:46:22 -0400

    Updated all with server communication

--- skip/front-end/Skip/src/pages/home/home.ts
+++ skip/front-end/Skip/src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WaiterPage } from '../waiter/waiter';
 import { SkipperPage } from '../skipper/skipper';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -9,15 +10,47 @@ import { SkipperPage } from '../skipper/skipper';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: HttpClient) {
 
   }
 
   pushWaiterPage() {
-    this.navCtrl.push(WaiterPage);
-  }
+      this.navCtrl.push(WaiterPage);
+    }
 
-  pushSkipperPage() {
-    this.navCtrl.push(SkipperPage);
+    pushSkipperPage() {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': 'origin, content-type, accept'
+        })
+      };
+
+      // let first = document.getElementById('first').value;
+      // let last = document.getElementById('last').value;
+
+      console.log(this.first);
+      console.log(this.last);
+
+      let postData = {
+        "fname": this.first,
+        "lname": this.last
+      }
+
+
+      this.http.post( "https://cors-anywhere.herokuapp.com/http://35.221.22.94/books/add", postData, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        })
+      }).subscribe(data => {
+          console.log(data['_body']);
+         }, error => {
+          console.log(error);
+        });
+
+
+      this.navCtrl.push(SkipperPage);
+    }
   }
-}
--- skip/front-end/Skip/src/pages/skipper/skipper.ts
+++ skip/front-end/Skip/src/pages/skipper/skipper.ts
@@ -14,27 +14,54 @@ export class SkipperPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad SkipperPage');
   }
-  
-  addOne(id) {
-    console.log(id);
-    let button = (<HTMLInputElement>document.getElementById(id));
-    console.log(button);
-    
-    let val = Number(button.value);
-    val += 1;
-    console.log(val);
-    button.value = String(val);
-    button.innerHTML = String(val);
-   }
 
-  removeOne(id) {
-    if (Number((<HTMLInputElement>document.getElementById(id)).value) > 0) {
-      let button = (<HTMLInputElement>document.getElementById(id));
-      let val = Number(button.value);
-      val -= 1;
-      console.log(val);
-      button.value = String(val);
-      button.innerHTML = String(val);
+  addOne(id) {
+     let button1 = (<HTMLInputElement>document.getElementById('quantity'));
+     let button2 = (<HTMLInputElement>document.getElementById('quantity1'));
+
+     let val1 = Number(button1.value);
+     let val2 = Number(button2.value);
+     if (id == "quantity") {
+       val1++;
+     } else {
+       val2++;
+     }
+
+     let element = document.getElementById('total');
+     element.innerHTML = "$" + String(2.19 * val1 + 3.05 * val2);
+
+     if (id == 'quantity') {
+       button1.value = String(val1);
+       button1.innerHTML = String(val1);
+     } else {
+       button2.value = String(val2);
+       button2.innerHTML = String(val2);
+     }
     }
-  }
-}
+
+    removeOne(id) {
+      if (Number((<HTMLInputElement>document.getElementById(id)).value) > 0) {
+        let button1 = (<HTMLInputElement>document.getElementById('quantity'));
+        let button2 = (<HTMLInputElement>document.getElementById('quantity1'));
+
+        let val1 = Number(button1.value);
+        let val2 = Number(button2.value);
+        if (id == "quantity") {
+          val1--;
+        } else {
+          val2--;
+        }
+
+        let element = document.getElementById('total');
+        element.innerHTML = "$" + String(2.19 * val1 + 3.05 * val2);
+
+        if (id == 'quantity') {
+          button1.value = String(val1);
+          button1.innerHTML = String(val1);
+        } else {
+          button2.value = String(val2);
+          button2.innerHTML = String(val2);
+        }
+      }
+    }
+ }
